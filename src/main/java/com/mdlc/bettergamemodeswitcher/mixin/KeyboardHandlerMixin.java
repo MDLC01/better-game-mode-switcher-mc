fc15fdf9a73206bcf4f4ec4683fe97a5643d3f0b
Malo <57839069+MDLC01@users.noreply.github.com>
2025-06-17 22:08:58 +0100

    Update to Minecraft 1.21.6

--- src/main/java/com/mdlc/bettergamemodeswitcher/mixin/KeyboardHandlerMixin.java
+++ src/main/java/com/mdlc/bettergamemodeswitcher/mixin/KeyboardHandlerMixin.java
@@ -4,8 +4,8 @@ import com.mojang.brigadier.tree.RootCommandNode;
 import net.minecraft.client.KeyboardHandler;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientPacketListener;
+import net.minecraft.client.multiplayer.ClientSuggestionProvider;
 import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.commands.SharedSuggestionProvider;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -18,8 +18,8 @@ public abstract class KeyboardHandlerMixin {
     @Shadow @Final private Minecraft minecraft;
 
     /**
-     * Redirects the {@link net.minecraft.world.entity.Entity#hasPermissions(int) hasPermissions} call in F3+N behavior
-     * to check whether the {@code gamemode} command is available instead.
+     * Redirects the {@link net.minecraft.world.entity.player.Player#hasPermissions(int) hasPermissions} call in F3+N
+     * behavior to check whether the {@code gamemode} command is available instead.
      */
     @Redirect(method = "handleDebugKeys", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/player/LocalPlayer;hasPermissions(I)Z", ordinal = 1))
     private boolean redirectF3NHasPermission(LocalPlayer player, int level) {
@@ -27,13 +27,13 @@ public abstract class KeyboardHandlerMixin {
         if (connection == null) {
             return true;
         }
-        RootCommandNode<SharedSuggestionProvider> root = connection.getCommands().getRoot();
+        RootCommandNode<ClientSuggestionProvider> root = connection.getCommands().getRoot();
         return root.getChild("gamemode") != null;
     }
 
     /**
-     * Redirects the {@link net.minecraft.world.entity.Entity#hasPermissions(int) hasPermissions} call in the game mode
-     * switcher's behavior to check whether the {@code gamemode} command is available instead.
+     * Redirects the {@link net.minecraft.world.entity.player.Player#hasPermissions(int) hasPermissions} call in the
+     * game mode switcher's behavior to check whether the {@code gamemode} command is available instead.
      */
     @Redirect(method = "handleDebugKeys", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/player/LocalPlayer;hasPermissions(I)Z", ordinal = 2))
     private boolean redirectF3F4HasPermission(LocalPlayer player, int level) {
@@ -41,7 +41,7 @@ public abstract class KeyboardHandlerMixin {
         if (connection == null) {
             return true;
         }
-        RootCommandNode<SharedSuggestionProvider> root = connection.getCommands().getRoot();
+        RootCommandNode<ClientSuggestionProvider> root = connection.getCommands().getRoot();
         return root.getChild("gamemode") != null;
     }
 }
